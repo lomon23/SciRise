b5bf66b4e17cd8185e49d2f42b6af23bfc443ae7
lomon23 <lomontav22@gmail.com>
2026-02-20 14:31:05 +0000

    deleted all project for new architecture

--- backend/myproject/main/views/__init__.py
+++
@@ -1,6 +0,0 @@
-from .login import login_api
-from .register import register_api
-from .note import list_notes, create_note, get_note, update_note, delete_note
-from .user import current_user
-from .chat import get_recent_chats, search_users, start_chat, get_chat_history
-from .auth import google_login
